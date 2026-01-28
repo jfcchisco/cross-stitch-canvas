2a629457796528a25c829f42383073f67548b61a
unknown <jfcchisco@gmail.com>
2026-01-28 20:22:33 +0100

    Adjusted pinch zoom

--- grid-manager.js
+++ grid-manager.js
@@ -876,7 +876,7 @@ class GridManager {
         } else {
             const zoomFactor = currentDistance / this.initialPinchDistance;
             console.log("Current distance:", currentDistance, "Zoom factor:", zoomFactor);
-            this.cameraZoom = zoomFactor;
+            this.cameraZoom = zoomFactor + this.lastZoom - 1;
 
             // Schedule a single render instead of rendering on every move
             if (!this.renderScheduled) {
